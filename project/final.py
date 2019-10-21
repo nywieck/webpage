38ea7e76be442d0a0ee690c3af729300ecfa3b75
Nathaniel Wieck <46864151+nywieck@users.noreply.github.com>
2019-10-20 22:25:28 -0700

    Add files via upload

--- project/final.py
+++ project/final.py
@@ -4,6 +4,13 @@ def main():
     playGame()
 
 def playGame():
+    print("""\n
+  _   _        _               _             
+ | | (_)      | |             | |            
+ | |_ _  ___  | |_ __ _  ___  | |_ ___   ___ 
+ | __| |/ __| | __/ _` |/ __| | __/ _ \ / _ \\
+ | |_| | (__  | || (_| | (__  | || (_) |  __/
+  \__|_|\___|  \__\__,_|\___|  \__\___/ \___|\n\n""")
     gameCount = 0
     compWin = 0
     playerWin = 0
@@ -24,31 +31,94 @@ def playGame():
         gameCount += 1
         if nextTurn == -1:
             compWin += 1
-            print('\nComputer wins!')
+            print("""\n\n
+                                  _                       _           
+                                 | |                     (_)          
+   ___ ___  _ __ ___  _ __  _   _| |_ ___ _ __  __      ___ _ __  ___ 
+  / __/ _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__| \ \ /\ / / | '_ \/ __|
+ | (_| (_) | | | | | | |_) | |_| | ||  __/ |     \ V  V /| | | | \__ \\
+  \___\___/|_| |_| |_| .__/ \__,_|\__\___|_|      \_/\_/ |_|_| |_|___/
+                     | |                                              
+                     |_|                                              
+            \n\n""")
         elif nextTurn == -2:
             playerWin += 1
-            print('\nPlayer wins!')
+            print("""\n\n
+        _                                  _           
+       | |                                (_)          
+  _ __ | | __ _ _   _  ___ _ __  __      ___ _ __  ___ 
+ | '_ \| |/ _` | | | |/ _ \ '__| \ \ /\ / / | '_ \/ __|
+ | |_) | | (_| | |_| |  __/ |     \ V  V /| | | | \__ \\
+ | .__/|_|\__,_|\__, |\___|_|      \_/\_/ |_|_| |_|___/
+ | |             __/ |                                 
+ |_|            |___/                                  
+            \n\n""")
         else:
-            print('\nDraw!')
-        print(f'\n# of games played: {gameCount}\n# of computer wins: {compWin}\n# of player wins: {playerWin}')
-        keepPlaying = int(input('\nPlay again (enter 1 or 2)?\n1: Yes, play again\n2: No, quit'))
-    print('\nThanks for playing!\n')
+            print("""\n\n
+      _                    
+     | |                   
+   __| |_ __ __ ___      __
+  / _` | '__/ _` \ \ /\ / /
+ | (_| | | | (_| |\ V  V / 
+  \__,_|_|  \__,_| \_/\_/  
+            \n\n""")
+        print(f'\nTotal games played: {gameCount}\nTotal computer wins: {compWin}\nTotal player wins: {playerWin}')
+        while True:
+            choice = input('\nPlay again (enter 1 or 2)?\n1: Yes, play again\n2: No, quit\n')
+            try:
+                keepPlaying = int(choice)
+                if keepPlaying < 1 or keepPlaying > 2:
+                    print("You have fat fingers - input must be the integer 1 or 2, try again!\n")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("You have fat fingers - input must be the integer 1 or 2, try again!\n")
+        
     if compWin > playerWin:
-        print(f'Computer wins the match, {compWin} games to {playerWin} games.\n')
+        print("""
+                                  _                       _                             _       _     
+                                 | |                     (_)                           | |     | |    
+   ___ ___  _ __ ___  _ __  _   _| |_ ___ _ __  __      ___ _ __  ___   _ __ ___   __ _| |_ ___| |__  
+  / __/ _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__| \ \ /\ / / | '_ \/ __| | '_ ` _ \ / _` | __/ __| '_ \ 
+ | (_| (_) | | | | | | |_) | |_| | ||  __/ |     \ V  V /| | | | \__ \ | | | | | | (_| | || (__| | | |
+  \___\___/|_| |_| |_| .__/ \__,_|\__\___|_|      \_/\_/ |_|_| |_|___/ |_| |_| |_|\__,_|\__\___|_| |_|
+                     | |                                                                              
+                     |_|                                                                              
+        \n""")
+        print(f'Computer wins ({compWin} games to {playerWin} games)\n')
     elif playerWin > compWin:
-        print(f'Player wins the match, {playerWin} games to {compWin} games.\n')
+        print("""
+        _                                  _                             _       _     
+       | |                                (_)                           | |     | |    
+  _ __ | | __ _ _   _  ___ _ __  __      ___ _ __  ___   _ __ ___   __ _| |_ ___| |__  
+ | '_ \| |/ _` | | | |/ _ \ '__| \ \ /\ / / | '_ \/ __| | '_ ` _ \ / _` | __/ __| '_ \ 
+ | |_) | | (_| | |_| |  __/ |     \ V  V /| | | | \__ \ | | | | | | (_| | || (__| | | |
+ | .__/|_|\__,_|\__, |\___|_|      \_/\_/ |_|_| |_|___/ |_| |_| |_|\__,_|\__\___|_| |_|
+ | |             __/ |                                                                 
+ |_|            |___/                                                                  
+        \n""")
+        print(f'Player wins ({playerWin} games to {compWin} games)\n')
     else:
-        print(f'Draw, {playerWin} games to {compWin} games.\n')
+        print("""
+      _                    
+     | |                   
+   __| |_ __ __ ___      __
+  / _` | '__/ _` \ \ /\ / /
+ | (_| | | | (_| |\ V  V / 
+  \__,_|_|  \__,_| \_/\_/  
+            \n""")
+        print(f'Draw, {playerWin} games each.\n')
+    print('Thanks for playing!\n')
 
 def coinToss():
-    print("\n\n\n\n_=[[  TIC - TAC - TOE  ]]=_\n\n-------- -------- --------\n")
-    print("So you know I am not cheating, flip a coin to decide who goes first, and I'll guess heads or tails!\n")
+    print("\nSo you know I am not cheating, flip a coin to decide who goes first, and I'll guess heads or tails!\n")
     input("Press Enter to continue...")
     guess = random.randrange(0, 2, 1)
     if guess == 0:
-        guess = "heads"
+        guess = "HEADS"
     else:
-        guess = "tails"
+        guess = "TAILS"
     print(f"\nI guessed {guess}!\n\nWho won and will be going first?\n")
     while True:
         firstTurn = input("Enter 1 or 2 to start game:\n1: Computer\n2: Player\n\n\n")
@@ -115,14 +185,22 @@ def endGame(board, player):
         return(0)
 
 def playerTurn(board):
-    choice = input("Choose your next move (enter the number): ")
     while True:
-        choice = int(choice)
-        if board[choice] == 0:
-            choiceDict = {choice: -1}
-            board.update(choiceDict)
-            break
-        choice = input("You cannot move there, the square is already taken - try again: ")
+        square = input("Choose your next move (enter the number): \n")
+        try:
+            choice = int(square)
+            if choice < 1 or choice > 9:
+                print("You have fat fingers - input must be an integer from 1 to 9!\n")
+                continue
+            elif board[choice] != 0:
+                print("You cannot move there, the square is already taken - try again!\n")
+                continue
+            else:
+                choiceDict = {choice: -1}
+                board.update(choiceDict)
+                break
+        except ValueError:
+            print("You have fat fingers - input must be an integer from 1 to 9!\n")
     drawBoard(board)
     return(endGame(board, 2))
 
@@ -131,6 +209,8 @@ def compTurn(board):
     drawBoard(board)
     return(endGame(board, 1))
 
+# if go first, can try to setup guaranteed win
+# if go second, must prevent impossible loss
 def decision(board):
     winCon1 = board[1] + board[4] + board[7]
     winCon2 = board[2] + board[5] + board[8]
@@ -152,15 +232,18 @@ def decision(board):
     winSquaresList = [winSquares1, winSquares2, winSquares3, winSquares4, winSquares5, winSquares6, winSquares7, winSquares8]
     choice = 0
     occupiedSquares = 0
-    # 1) EVAL FIRST TURN, SECOND TURN
     for i in range(1, 10, 1):
         if board[i] != 0:
             occupiedSquares += 1
+
+    # 1) EVAL FIRST TURN
     if occupiedSquares == 0:
         choiceDict = {5:1}
         board.update(choiceDict)
         return(board)
-    elif occupiedSquares == 1:
+
+    # 2) EVAL SECOND TURN
+    if occupiedSquares == 1:
         if board[5] == 0:
             choiceDict = {5:1}
             board.update(choiceDict)
@@ -170,7 +253,7 @@ def decision(board):
             board.update(choiceDict)
             return(board)
 
-    # 2) EVAL IF CAN WIN THIS TURN
+    # 2) ALWAYS EVAL IF CAN WIN THIS TURN
     for i in range(0, 8, 1):
         if winConList[i] == 2:
             for j in winSquaresList[i]:
@@ -179,7 +262,7 @@ def decision(board):
                     board.update(choiceDict)
                     return(board)
 
-    # 3) EVAL IF NEED TO PREVENT LOSS THIS TURN
+    # 3) ALWAYS EVAL IF NEED TO PREVENT LOSS THIS TURN
     for i in range(0, 8, 1):
         if winConList[i] == -2:
             for j in winSquaresList[i]:
@@ -188,49 +271,165 @@ def decision(board):
                     board.update(choiceDict)
                     return(board)
 
-    # 4) EVAL IF NEED TO PREVENT LOSS NEXT TURN
-    prevent1 = [[1, 5, 9], [3, 5, 7], [9, 5, 1], [7, 5, 3]]
-    for layout in prevent1:
-        if board[layout[0]] == 1 and board[layout[1]] == -1 and board[layout[2]] == -1:
-            if board[1] == 0:
-                choiceDict = {1:1}
+    # 4) EVAL THIRD TURN
+    # <<<<<<<  if occupiedSquares == 2, then means can be aggressive don't need worry about unwinnable, try to setup own >>>>>>>
+    # <<<<<<<  phase 1 is in third turn, phase 2 is in fifth turn if works out, and last move in turn 7 will be auto by above code >>>>>>>
+    # A
+    if occupiedSquares == 2:
+        if board[5] == 1:
+            if board[6] == -1:
+                choiceDict = {4:1}
                 board.update(choiceDict)
                 return(board)
-            elif board[1] != 0:
-                choiceDict = {3:1}
-                board.update(choiceDict)
-                return(board)
-        elif board[layout[0]] == -1 and board[layout[1]] == 1 and board[layout[2]] == -1:
+            elif board[8] == -1:
                 choiceDict = {2:1}
                 board.update(choiceDict)
                 return(board)
+            elif board[4] == -1:
+                choiceDict = {6:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[2] == -1:
+                choiceDict = {8: 1}
+                board.update(choiceDict)
+                return(board)
+            # B
+            elif board[1] == -1:
+                choiceDict = {9: 1}
+                board.update(choiceDict)
+                return(board)
+            elif board[3] == -1:
+                choiceDict = {7: 1}
+                board.update(choiceDict)
+                return(board)
+            elif board[7] == -1:
+                choiceDict = {3: 1}
+                board.update(choiceDict)
+                return(board)
+            elif board[9] == -1:
+                choiceDict = {1: 1}
+                board.update(choiceDict)
+                return(board)
 
-    # 5) PREVENT guaranteed loss in next turn turns
-    prevent2 = [[7, 5, 6], [1, 5, 6], [1, 5, 8], [3, 5, 8], [4, 5, 9], [4, 5, 3], [2, 5, 7], [2, 5, 9]]
-    if (board[prevent2[0][0]] == -1 and board[prevent2[0][1]] == 1 and board[prevent2[0][2]] == -1) or \
-       (board[prevent2[3][0]] == -1 and board[prevent2[3][1]] == 1 and board[prevent2[3][2]] == -1):
-       choiceDict = {1:1}
-       board.update(choiceDict)
-       return(board)
-    elif (board[prevent2[2][0]] == -1 and board[prevent2[2][1]] == 1 and board[prevent2[2][2]] == -1) or \
-         (board[prevent2[4][0]] == -1 and board[prevent2[4][1]] == 1 and board[prevent2[4][2]] == -1):
-         choiceDict = {3:1}
-         board.update(choiceDict)
-         return(board)
-    elif (board[prevent2[1][0]] == -1 and board[prevent2[1][1]] == 1 and board[prevent2[1][2]] == -1) or \
-         (board[prevent2[7][0]] == -1 and board[prevent2[7][1]] == 1 and board[prevent2[7][2]] == -1):
-         choiceDict = {7:1}
-         board.update(choiceDict)
-         return(board)
-    elif (board[prevent2[5][0]] == -1 and board[prevent2[5][1]] == 1 and board[prevent2[5][2]] == -1) or \
+        # C (would have to program differently, because currently if go first always mid, and if second always square #3)
+        # D (would have to program differently, because currently if go first always mid, and if second always square #3)
+        # E (would have to program differently, because currently if go first always mid, and if second always square #3)
+
+    # <<<<<<<  if occupiedSquares == 2, then means must be careful about unwinnable conditions, must prevent now   >>>>>>>
+
+    # 5) EVAL FOURTH TURN - PREVENT guaranteed loss in next TWO turns
+    if occupiedSquares == 3:
+        prevent1 = [[1, 5, 9], [3, 5, 7], [9, 5, 1], [7, 5, 3]]
+        for layout in prevent1:
+            if board[layout[0]] == 1 and board[layout[1]] == -1 and board[layout[2]] == -1:
+                if board[1] == 0:
+                    choiceDict = {1:1}
+                    board.update(choiceDict)
+                    return(board)
+                elif board[1] != 0:
+                    choiceDict = {3:1}
+                    board.update(choiceDict)
+                    return(board)
+            elif board[layout[0]] == -1 and board[layout[1]] == 1 and board[layout[2]] == -1:
+                    choiceDict = {2:1}
+                    board.update(choiceDict)
+                    return(board)
+
+        prevent2 = [[7, 5, 6], [1, 5, 6], [1, 5, 8], [3, 5, 8], [4, 5, 9], [4, 5, 3], [2, 5, 7], [2, 5, 9]]
+        if (board[prevent2[0][0]] == -1 and board[prevent2[0][1]] == 1 and board[prevent2[0][2]] == -1) or \
+           (board[prevent2[3][0]] == -1 and board[prevent2[3][1]] == 1 and board[prevent2[3][2]] == -1):
+                choiceDict = {1:1}
+                board.update(choiceDict)
+                return(board)
+        elif (board[prevent2[2][0]] == -1 and board[prevent2[2][1]] == 1 and board[prevent2[2][2]] == -1) or \
+             (board[prevent2[4][0]] == -1 and board[prevent2[4][1]] == 1 and board[prevent2[4][2]] == -1):
+                choiceDict = {3:1}
+                board.update(choiceDict)
+                return(board)
+        elif (board[prevent2[1][0]] == -1 and board[prevent2[1][1]] == 1 and board[prevent2[1][2]] == -1) or \
+             (board[prevent2[7][0]] == -1 and board[prevent2[7][1]] == 1 and board[prevent2[7][2]] == -1):
+                choiceDict = {7:1}
+                board.update(choiceDict)
+                return(board)
+        elif (board[prevent2[5][0]] == -1 and board[prevent2[5][1]] == 1 and board[prevent2[5][2]] == -1) or \
          (board[prevent2[6][0]] == -1 and board[prevent2[6][1]] == 1 and board[prevent2[6][2]] == -1):
-         choiceDict = {9:1}
-         board.update(choiceDict)
-         return(board)
+                choiceDict = {9:1}
+                board.update(choiceDict)
+                return(board)
 
-    # 6) EVAL if computer can setup guaranteed win 2 turns later
-    
+        prevent3 = [[5, 6, 8], [5, 2, 4], [5, 4, 8], [5 ,2, 6]]
+        if (board[prevent2[0][0]] == 1 and board[prevent2[0][1]] == -1 and board[prevent2[0][2]] == -1):
+            choiceDict = {9:1}
+            board.update(choiceDict)
+            return(board)
+        elif (board[prevent2[1][0]] == 1 and board[prevent2[1][1]] == -1 and board[prevent2[1][2]] == -1):
+            choiceDict = {1:1}
+            board.update(choiceDict)
+            return(board)
+        elif (board[prevent2[2][0]] == 1 and board[prevent2[2][1]] == -1 and board[prevent2[2][2]] == -1):
+            choiceDict = {7:1}
+            board.update(choiceDict)
+            return(board)
+        elif (board[prevent2[3][0]] == 1 and board[prevent2[3][1]] == -1 and board[prevent2[3][2]] == -1):
+            choiceDict = {3:1}
+            board.update(choiceDict)
+            return(board)
 
+    # 6) FIFTH TURN - COMPLETE setting up guaranteed win if went first here
+    # A
+    if occupiedSquares == 4:
+        if board[5] == 1:
+            if board[4] == 1 and board[6] == -1 and board[8] == -1:
+                choiceDict = {1:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[4] == 1 and board[2] == -1 and board[6] == -1:
+                choiceDict = {7:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[2] == 1 and board[6] == -1 and board[8] == -1:
+                choiceDict = {1:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[2] == 1 and board[4] == -1 and board[8] == -1:
+                choiceDict = {3:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[6] == 1 and board[2] == -1 and board[4] == -1:
+                choiceDict = {9:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[6] == 1 and board[4] == -1 and board[8] == -1:
+                choiceDict = {3:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[8] == 1 and board[2] == -1 and board[4] == -1:
+                choiceDict = {9:1}
+                board.update(choiceDict)
+                return(board)
+            elif board[8] == 1 and board[2] == -1 and board[6] == -1:
+                choiceDict = {7:1}
+                board.update(choiceDict)
+                return(board)
+            # B 
+            elif (board[1] == -1 and board[8] == -1 and board[6] == 1) or (board[3] == -1 and board[4] == -1 and board[7] == 1):
+                choiceDict = {8:1}
+                board.update(choiceDict)
+                return(board)
+            elif (board[9] == -1 and board[4] == -1 and board[1] == 1) or (board[7] == -1 and board[6] == -1 and board[3] == 1):
+                choiceDict = {2:1}
+                board.update(choiceDict)
+                return(board)
+            elif (board[9] == -1 and board[1] == -1 and board[1] == 1) or (board[3] == -1 and board[8] == -1 and board[7] == 1):
+                choiceDict = {4:1}
+                board.update(choiceDict)
+                return(board)
+            elif (board[1] == -1 and board[8] == -1 and board[9] == 1) or (board[7] == -1 and board[2] == -1 and board[3] == 1):
+                choiceDict = {6:1}
+                board.update(choiceDict)
+                return(board)
+
+    # <<<<<<< WHERE TO GO IF NONE OF THE ABOVE, NOT DETERMINED BY THE OCCUPIED SQUARES TURN COUNTER >>>>>>>
     # 7) EVAL where to go if all other above conditions not met
     for i in range(1, 10):
         if board[i] == 0:
